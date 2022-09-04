efb22b3df1719078be91c193f6f4e2cdeebe5a35
marlon <maryesacu@gmail.com>
2022-09-04 18:29:35 -0500

    first step

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Content />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
